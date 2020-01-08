ff6ede46d35e8d0f88db33fafe7f815cd3e775bb
HouserChiu <18391726137@163.com>
2020-01-08 23:07:41 +0800

    other commit

--- wechat_spider.py
+++ wechat_spider.py
@@ -2,8 +2,9 @@ import time
 
 from appium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 class Wechat_Moment():
   def __init__(self):
