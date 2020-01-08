d2999896fcf992d6cc4e52f0ebb782c47e7bf70b
HouserChiu <18391726137@163.com>
2020-01-08 23:03:51 +0800

    demo commit

--- wechat_spider.py
+++ wechat_spider.py
@@ -1,7 +1,7 @@
 import time
 
-from appium import webdriver
 from selenium.webdriver.common.by import By
+from appium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
